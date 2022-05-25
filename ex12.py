6e72ea48669b70c4a6a7524466bad311fd087fa4
igor.rabelo <igor.rabelo@coopersystem.com.br>
2022-05-25 15:32:06 -0300

    retornar mensagem e número float

---
+++ ex12.py
@@ -0,0 +1,6 @@
+
+def retornar_mensagem(mensagem):
+    return input(mensagem)
+
+def retornar_numero_float(numero):
+    return float(input(numero))
---
+++ ex13.py
@@ -0,0 +1,7 @@
+import ex12
+
+msg=ex12.retornar_mensagem('Digite a mensagem: ')
+nf=ex12.retornar_numero_float('Digite o valor: ')
+
+print(msg)
+print(nf)
