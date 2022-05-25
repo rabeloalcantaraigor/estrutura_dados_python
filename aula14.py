76c759f6e7cfe223e2e5905aa76b4773d6ceb1b3
igor.rabelo <igor.rabelo@coopersystem.com.br>
2022-05-25 15:59:45 -0300

    aula sobre tratamento de erros com exceções

---
+++ aula14.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        n=int(input('Digite um número inteiro: '))
+    
+    except:
+        print('Valor inválido.')
+
+    else:    
+        print(f'Valor digitado é {n}')
+        break;
