82c249295ff830650f64bda97203fa68adc1ce83
igor.rabelo <igor.rabelo@coopersystem.com.br>
2022-06-01 11:19:02 -0300

    aula de recursão para somar valores

--- recursao/aula2.py
+++ recursao/aula2.py
@@ -1,3 +1,19 @@
 
-# Recursão Aula 2
+# Recursão Aula 2 - Somar valores
+
+def soma1(n):
+    soma = 0
+    for i in range(n + 1):
+        soma += i
+    return soma
+
+print(soma1(5))
+
+def soma2(n):
+    if n == 0:
+        return 0
+    
+    return n + soma2(n - 1)
+
+print(soma2(5))
 
